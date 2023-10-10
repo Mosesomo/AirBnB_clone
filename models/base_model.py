ee61ee20ee2063bbf6213aec983324deb79139fc
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-10-10 12:30:31 -0700

    e-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This module define a class BaseModel
+"""Defines the BaseModel class."""
 
 import uuid
 from datetime import datetime
@@ -8,12 +8,23 @@ from datetime import datetime
 class BaseModel:
     """Representing the base class model"""
 
-    def __init__(self):
-        """This function initializes instance attributes"""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """prints the string representation"""
@@ -22,14 +33,13 @@ class BaseModel:
                 format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        """updates the public instance attribute
-            updated_at with the current datetime
+        """updates the public instance attribute updated_at with the current datetime
         """
 
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of
+        """returns a dictionary containing all keys/values of
             __dict__ of the instance:
         """
 
